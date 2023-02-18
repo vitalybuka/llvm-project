f1124364336b8bfa341e8a39caba1170205cb6c3
Vitaly Buka <vitalybuka@google.com>
2023-02-18 14:32:38 -0800

    SmallFix

--- llvm/include/llvm/Analysis/ScalarEvolution.h
+++ llvm/include/llvm/Analysis/ScalarEvolution.h
@@ -1297,7 +1297,7 @@ public:
   bool loopIsFiniteByAssumption(const Loop *L);
 
   class FoldID {
-    SmallVector<unsigned, 4> Bits;
+    SmallVector<unsigned, 2 * sizeof(void*)/sizeof(unsigned) + 1> Bits;
 
   public:
     void addInteger(unsigned long I) {
