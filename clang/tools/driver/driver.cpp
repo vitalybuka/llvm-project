2e5dc7ade2bb7ed034de06e339fdccf481ec2055
Vitaly Buka <vitalybuka@gmail.com>
2023-02-19 20:15:49 -0800

    builtin

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -429,7 +429,7 @@ int clang_main(int Argc, char **Argv, const llvm::ToolContext &ToolContext) {
   }
 
   Args.push_back("-fsanitize=undefined");
-  Args.push_back("-fno-sanitize=alignment,bool");
+  Args.push_back("-fno-sanitize=alignment,bool,builtin");
 
   // Handle -cc1 integrated tools, even if -cc1 was expanded from a response
   // file.
