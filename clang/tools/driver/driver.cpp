a4537ec8afcb321b0af9230875c9e80f78afbbdf
Vitaly Buka <vitalybuka@gmail.com>
2023-02-19 20:16:38 -0800

    bounds

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -260,7 +260,7 @@ int clang_main(int Argc, char **Argv, const llvm::ToolContext &ToolContext) {
   }
 
   Args.push_back("-fsanitize=undefined");
-  Args.push_back("-fno-sanitize=alignment,bool,builtin");
+  Args.push_back("-fno-sanitize=alignment,bool,builtin,bounds");
 
   // Handle -cc1 integrated tools.
   if (Args.size() >= 2 && StringRef(Args[1]).starts_with("-cc1"))
