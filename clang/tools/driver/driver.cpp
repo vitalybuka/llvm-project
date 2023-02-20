77f53b62abea7cf540435ebd1a17967e4d21ba59
Vitaly Buka <vitalybuka@gmail.com>
2023-02-19 20:24:18 -0800

    unsigned-shift-base

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -260,7 +260,7 @@ int clang_main(int Argc, char **Argv, const llvm::ToolContext &ToolContext) {
   }
 
   Args.push_back("-fsanitize=undefined");
-  Args.push_back("-fno-sanitize=alignment,bool,builtin,bounds,enum,float-cast-overflow,float-divide-by-zero,function,implicit-unsigned-integer-truncation,implicit-signed-integer-truncation,integer-divide-by-zero,nonnull-attribute,null,nullability-arg,nullability-assign,nullability-return,objc-cast,object-size,pointer-overflow,return,returns-nonnull-attribute,shift");
+  Args.push_back("-fno-sanitize=alignment,bool,builtin,bounds,enum,float-cast-overflow,float-divide-by-zero,function,implicit-unsigned-integer-truncation,implicit-signed-integer-truncation,integer-divide-by-zero,nonnull-attribute,null,nullability-arg,nullability-assign,nullability-return,objc-cast,object-size,pointer-overflow,return,returns-nonnull-attribute,shift,unsigned-shift-base");
 
   // Handle -cc1 integrated tools.
   if (Args.size() >= 2 && StringRef(Args[1]).starts_with("-cc1"))
