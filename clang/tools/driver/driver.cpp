6aa0cf1e98186ae124efba4d855a61a8ff46db3e
Vitaly Buka <vitalybuka@gmail.com>
2023-02-19 20:14:27 -0800

    Update driver.cpp

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -260,6 +260,7 @@ int clang_main(int Argc, char **Argv, const llvm::ToolContext &ToolContext) {
   }
 
   Args.push_back("-fsanitize=undefined");
+  Args.push_back("-fno-sanitize=alignment");
 
   // Handle -cc1 integrated tools.
   if (Args.size() >= 2 && StringRef(Args[1]).starts_with("-cc1"))
