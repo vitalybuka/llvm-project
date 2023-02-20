80c0a3ffea84e5171483a310ad13747518e247db
Vitaly Buka <vitalybuka@gmail.com>
2023-02-19 20:17:34 -0800

    float-cast-overflow

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -260,7 +260,7 @@ int clang_main(int Argc, char **Argv, const llvm::ToolContext &ToolContext) {
   }
 
   Args.push_back("-fsanitize=undefined");
-  Args.push_back("-fno-sanitize=alignment,bool,builtin,bounds,enum");
+  Args.push_back("-fno-sanitize=alignment,bool,builtin,bounds,enum,float-cast-overflow");
 
   // Handle -cc1 integrated tools.
   if (Args.size() >= 2 && StringRef(Args[1]).starts_with("-cc1"))
